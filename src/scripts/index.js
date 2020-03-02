526f3b81397f31bf26acad1be256f9008db5cea8
Bartłomiej Kochanowicz <bartlomiej.kochanowicz@gmail.com>
2020-03-02 23:37:56 +0100

    done layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,7 @@ import '../styles/reset.css';
 import '../styles/common.css';
 import '../styles/index.css';
 import '../styles/components/header.css';
+import '../styles/components/information.css';
+import '../styles/components/products.css';
 
 console.log('webpack starterkit');
